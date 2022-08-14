ee4b59c9f916e56fe66f690ef7a4ef86d77603fe
LiamMaclean216 <lmaclean234@gmaill.com>
2022-08-13 21:07:01 -0400

    early SelfOrganizeTest

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import torch
 # type_dict = {"hidden": 0, "input": 1, "output": 2}
 type_dict = {"hidden": [1, 0, 0], "input": [0, 1, 0], "output": [0, 0, 1]}
 
-def reset_inputs(x_data: torch.Tensor, input_data: torch.Tensor, hidden_dim: int = 1):
+def reset_inputs(x_data: torch.Tensor, input_data: torch.Tensor, output_data: torch.Tensor = None, hidden_dim: int = 1):
     """
     x_data: [n_nodes, n_features]
         features: [0:-2] is data
@@ -12,6 +12,11 @@ def reset_inputs(x_data: torch.Tensor, input_data: torch.Tensor, hidden_dim: int
     # x_data[torch.argwhere(x_data[:,1] == type_dict["input"]).squeeze(-1), [0]] = input_data
     # print(x_data[torch.argwhere((x_data[:,1:] == torch.tensor(type_dict["input"])).all(1)).squeeze(-1), [0]].shape)
     x_data[torch.argwhere((x_data[:,hidden_dim:] == torch.tensor(type_dict["input"])).all(1)).squeeze(-1), [0]] = input_data
+    
+    if output_data is not None:
+        x_data[torch.argwhere((x_data[:,hidden_dim:] == torch.tensor(type_dict["output"])).all(1)).squeeze(-1), [0]] = output_data
+
+        
     return x_data
 
 def get_output(data, hidden_dim: int = 1):
