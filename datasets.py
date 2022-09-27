66fb828d0845b12ac51a8181c48c9fd2e9c67328
LiamMaclean216 <lmaclean234@gmail.com>
2022-09-27 14:46:32 -0400

    work

--- datasets.py
+++ datasets.py
@@ -1,5 +1,7 @@
 import torch
 from torch.nn.functional import one_hot
+from torch.utils.data import DataLoader
+
 
 class SelfOrganizeTest(torch.nn.Module):
     """
@@ -39,21 +41,55 @@ class XORDataset(torch.utils.data.Dataset):
         return len(self.data)
     
     
-class TranslateDataset(torch.utils.data.Dataset):
+class Dataset(torch.utils.data.Dataset):
+    """
+    datset superclass
+    """
+    def __init__(self, data, target):
+        self.data = data
+        self.target = target
+        
+    def __getitem__(self, index):
+        return self.data[index], self.target[index]
+
+    def __len__(self):
+        return len(self.data)    
+
+n = 4
+drop = 3
+
+class TranslateDataset(Dataset):
     def __init__(self) -> None:
-        super().__init__()
-        n = 7
-        drop = 3
         
         arr = torch.arange(n)
 
         if drop:
             arr = torch.cat([arr[:drop-1], arr[drop:]])
     
-        self.data = one_hot(arr, n)
-        self.target = one_hot((arr+1)%n, n)
+        super().__init__(one_hot(arr, n), one_hot((arr+1)%n, n))
+
+
+class TranslateDataset2(Dataset):
+    def __init__(self) -> None:
+        arr = torch.arange(n)
+        if drop:
+            arr = torch.cat([arr[:drop-1], arr[drop:]])
+    
+        super().__init__(one_hot(arr, n), one_hot((arr-1)%n, n))
         
-    def __getitem__(self, index):
-        return self.data[index], self.target[index]
-    def __len__(self):
-        return len(self.data)
+    
+class MetaDataset():
+    """
+    Dataset of datasets
+    """
+    def __init__(self):
+        self.datasets = [dataset() for dataset in [TranslateDataset, TranslateDataset2]]
+        
+    def iterate(self):
+        """
+        returns a generator that gives an index
+        """
+        yield from range(len(self.datasets))
+        
+    def get_set(self, n):
+        return DataLoader(self.datasets[n] , batch_size=1, shuffle=True)
